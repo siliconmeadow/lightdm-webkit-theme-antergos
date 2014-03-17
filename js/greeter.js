32c359cf4f0e3761fbefff20860ee45f97e9b694
Dustin Falgout <dustin@falgout.us>
2014-03-17 01:13:13 -0500

    Show user list when any key is pressed; Removed some old/un-used functions

--- js/greeter.js
+++ js/greeter.js
@@ -4,47 +4,46 @@ var selectedUser = null;
 /**
  * UI Initialization.
  */
-$(document).ready(function() {
+$(document).ready(function () {
 
     initialize_timer();
     get_hostname();
-    
-	// User list building
-	var userList = document.getElementById('user-list');
 
-	for (i in lightdm.users)
-	{
-		user = lightdm.users[i];
+    // User list building
+    var userList = document.getElementById('user-list');
+
+    for (i in lightdm.users) {
+        user = lightdm.users[i];
         var tux = 'img/antergos-logo-user.png';
-		var imageSrc = user.image.length > 0 ? user.image : tux;
-		var li = '<li id="' + user.name + '">' +
-			'<a href="#' + user.name + '" onclick="startAuthentication(\'' + user.name + '\')">' +
-			'<em><span>' + user.display_name + '</span></em>' + 
-			'<img src="' + imageSrc + '" class="img-circle" alt="' + user.display_name + '" onerror="imgNotFound(this)"/> ' +
-			'</a>' +
-	   		'</li>';
-		$(userList).append(li);
-	}
-	
-	// Password key trigger registering
-	$("#passwordField").keypress(function() {
-		log("keypress(" + event.which + ")");
-		if (event.which == 13) { // 'Enter' key
-			submitPassword();
-		}
+        var imageSrc = user.image.length > 0 ? user.image : tux;
+        var li = '<li id="' + user.name + '">' +
+            '<a href="#' + user.name + '" onclick="startAuthentication(\'' + user.name + '\')">' +
+            '<em><span>' + user.display_name + '</span></em>' +
+            '<img src="' + imageSrc + '" class="img-circle" alt="' + user.display_name + '" onerror="imgNotFound(this)"/> ' +
+            '</a>' +
+            '</li>';
+        $(userList).append(li);
+    }
 
-	});
-	$(document).keyup(function(e) {
-		if (e.keyCode == 27) { // 'Escape' char
-			cancelAuthentication();
-		}
-	});
-	
-	// Action buttons
-	addActionLink("shutdown");
-	addActionLink("hibernate");
-	addActionLink("suspend");
-	addActionLink("restart");
+    // Password key trigger registering
+    $("#passwordField").keypress(function () {
+        log("keypress(" + event.which + ")");
+        if (event.which == 13) { // 'Enter' key
+            submitPassword();
+        }
+
+    });
+    $(document).keyup(function (e) {
+        if (e.keyCode == 27) { // 'Escape' char
+            cancelAuthentication();
+        }
+    });
+
+    // Action buttons
+    addActionLink("shutdown");
+    addActionLink("hibernate");
+    addActionLink("suspend");
+    addActionLink("restart");
 
 });
 
@@ -62,42 +61,45 @@ function get_hostname() {
 
 
 function update_time() {
-    var time= document.getElementById("current_time");
-    var date= new Date();
+    var time = document.getElementById("current_time");
+    var date = new Date();
 
-    var twelveHr= [
-            'sq-AL.UTF-8',
-            'zh-CN.UTF-8',
-            'zh-TW.UTF-8',
-            'en-AU.UTF-8',
-            'en-BZ.UTF-8',
-            'en-CA.UTF-8',
-            'en-CB.UTF-8',
-            'en-JM.UTF-8',
-            'en-NG.UTF-8',
-            'en-NZ.UTF-8',
-            'en-PH.UTF-8',
-            'en-US.UTF-8',
-            'en-TT.UTF-8',
-            'en-ZW.UTF-8',
-            'es-US.UTF-8',
-            'es-MX.UTF-8'];
-var userLang = navigator.language || navigator.userLanguage;
-var a = twelveHr.indexOf(userLang) ;
-
+    var twelveHr = [
+        'sq-AL',
+        'zh-CN',
+        'zh-TW',
+        'en-AU',
+        'en-BZ',
+        'en-CA',
+        'en-CB',
+        'en-JM',
+        'en-NG',
+        'en-NZ',
+        'en-PH',
+        'en-US',
+        'en-TT',
+        'en-ZW',
+        'es-US',
+        'es-MX'];
+    var userLang = navigator.language;
+    var a = twelveHr.indexOf(userLang);
     var hh = date.getHours();
     var mm = date.getMinutes();
-    var ss = date.getSeconds();
-    var suffix= "AM";
+    var suffix = "AM";
     if (hh > 12) {
-    	if(a !== -1){
-    		hh = hh - 12;
-    	}
-        suffix= "PM";
+        if (a !== -1) {
+            hh = hh - 12;
+        }
+        suffix = "PM";
     }
-    if (mm < 10) {mm = "0"+mm;}
-    if (ss < 10) {ss = "0"+ss;}
-    if ((hh === 0)&& (a !== -1)) {hh = "12";}
+    if (mm < 10) {
+        mm = "0" + mm;
+    }
+    if (hh === 0) {
+        if (a !== -1) {
+            hh = "12";
+        }
+    }
     time.innerHTML = hh + ":" + mm + " " + suffix;
 }
 
@@ -107,58 +109,65 @@ function initialize_timer() {
 }
 
 function addActionLink(id) {
-	if (eval("lightdm.can_" + id)) {
-		var label = id.substr(0,1).toUpperCase() + id.substr(1,id.length-1);
+    if (eval("lightdm.can_" + id)) {
+        var label = id.substr(0, 1).toUpperCase() + id.substr(1, id.length - 1);
         var id2;
-        if (id == "shutdown") {id2 = "power-off"}
-        if (id == "hibernate") {id2 = "asterisk"}
-        if (id == "suspend") {id2 = "arrow-down"}
-        if (id == "restart") {id2 = "refresh"}
-		$("#actionsArea").append('\n<button type="button" class="btn btn-default ' + id + '" title="' + label + '" onclick="handleAction(\'' + id + '\')"><i class="fa fa-' + id2 + '"></i></button>');
-	}
+        if (id == "shutdown") {
+            id2 = "power-off"
+        }
+        if (id == "hibernate") {
+            id2 = "asterisk"
+        }
+        if (id == "suspend") {
+            id2 = "arrow-down"
+        }
+        if (id == "restart") {
+            id2 = "refresh"
+        }
+        $("#actionsArea").append('\n<button type="button" class="btn btn-default ' + id + '" title="' + label + '" onclick="handleAction(\'' + id + '\')"><i class="fa fa-' + id2 + '"></i></button>');
+    }
 }
 
 function handleAction(id) {
-	log("handleAction(" + id + ")");
-	eval("lightdm." + id + "()");
+    log("handleAction(" + id + ")");
+    eval("lightdm." + id + "()");
 }
 
 function startAuthentication(userId) {
-	log("startAuthentication(" + userId + ")");
-	cancelAuthentication();
-	selectedUser = userId;
-	lightdm.start_authentication(selectedUser);
+    log("startAuthentication(" + userId + ")");
+    cancelAuthentication();
+    selectedUser = userId;
+    lightdm.start_authentication(selectedUser);
 }
 
 function cancelAuthentication() {
-	log("cancelAuthentication()");
-	$('#statusArea').hide();
-	$('#timerArea').hide();
-	if (selectedUser != null) {
-		lightdm.cancel_authentication();
-		log("authentication cancelled for " + selectedUser);
-		selectedUser = null;
-	}
-	return true;
+    log("cancelAuthentication()");
+    $('#statusArea').hide();
+    $('#timerArea').hide();
+    if (selectedUser != null) {
+        lightdm.cancel_authentication();
+        log("authentication cancelled for " + selectedUser);
+        selectedUser = null;
+    }
+    return true;
 }
 
-function submitPassword()
-{
-	log("provideSecret()");
-	lightdm.provide_secret($('#passwordField').val());
-	$('#passwordArea').hide();
-	$('#timerArea').show();
-	log("done");
+function submitPassword() {
+    log("provideSecret()");
+    lightdm.provide_secret($('#passwordField').val());
+    $('#passwordArea').hide();
+    $('#timerArea').show();
+    log("done");
 }
 
 /**
  * Image loading management.
  */
 
-function imgNotFound(source){
-	source.src = 'img/antergos-logo-user.png';
-	source.onerror = "";
-	return true;
+function imgNotFound(source) {
+    source.src = 'img/antergos-logo-user.png';
+    source.onerror = "";
+    return true;
 }
 
 
@@ -166,31 +175,31 @@ function imgNotFound(source){
  * Lightdm Callbacks
  */
 function show_prompt(text) {
-	log("show_prompt(" + text + ")");
-	$('#passwordField').val("");
-	$('#passwordArea').show();
-	$('#passwordField').focus();
+    log("show_prompt(" + text + ")");
+    $('#passwordField').val("");
+    $('#passwordArea').show();
+    $('#passwordField').focus();
 }
 
 function authentication_complete() {
-	log("authentication_complete()");
-	$('#timerArea').hide();
-	if (lightdm.is_authenticated) {
-		log("authenticated !");
-		lightdm.login (lightdm.authentication_user, lightdm.default_session);
-	}
-	else {
-		log("not authenticated !");
-		$('#statusArea').show();
-	}
+    log("authentication_complete()");
+    $('#timerArea').hide();
+    if (lightdm.is_authenticated) {
+        log("authenticated !");
+        lightdm.login(lightdm.authentication_user, lightdm.default_session);
+    }
+    else {
+        log("not authenticated !");
+        $('#statusArea').show();
+    }
 }
 
 /**
  * Logs.
  */
 function log(text) {
-	if (DEBUG) {
-		$('#logArea').append(text);
-		$('#logArea').append('<br/>');
-	}
+    if (DEBUG) {
+        $('#logArea').append(text);
+        $('#logArea').append('<br/>');
+    }
 }
